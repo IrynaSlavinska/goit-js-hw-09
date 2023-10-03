b798327956e7dfe832c2e12bfef3306f3df0ce46
IrynaSlavinska <133566139+IrynaSlavinska@users.noreply.github.com>
2023-10-03 11:58:27 +0300

    03.2

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,17 +1,57 @@
 import Notiflix from 'notiflix';
 
 const formEl = document.querySelector('.form');
-const submitBtn = document.querySelector('button');
-let counter = 1;
-let timerId = null;
 
-submitBtn.addEventListener('submit', onSubmitClick);
+formEl.addEventListener('submit', onSubmitClick);
+
+function onSubmitClick(evt) {
+  evt.preventDefault();
+  const { delay, step, amount } = evt.currentTarget.elements;
+
+  if (delay.value < 0 || step.value < 0 || amount.value < 0) {
+    Notiflix.Notify.warning('Enter more then 0');
+  }
+  for (let s = 0; s < amount.value; s += 1) {
+    let position = s + 1;
+    const intervalDelay = Number(delay.value) + step.value * s;
+    createPromise(position, intervalDelay)
+      .then(({ position, delay }) =>
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        )
+      )
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+  evt.currentTarget.reset();
+}
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+// if (shouldResolve) {
+//
+// } else {
+//
+// }
+// }
+
+// логіка
+// 1. натискаємо на самбіт
+// 1.2. дані з інпутів відправляються значеннями змінних
+//
+
+// 1.
